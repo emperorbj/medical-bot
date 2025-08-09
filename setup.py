e652e0172a4666a761d2705302a5a9ad4072d895
emperorbj <cwaku96@gmail.com>
2025-08-09 01:10:13 +0100

    added and install dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical bot",
+    version="0.1.0",
+    Author_name = "meddoctor",
+    Author_email="cwaku96@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
